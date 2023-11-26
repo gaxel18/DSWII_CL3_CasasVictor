d136015e6ffac1328f8557d802754269c831778d
gaxel18 <alejandro_cs_09@hotmail.com>
2023-11-25 20:29:32 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL3_CasasVictor/DswiiCl3CasasVictorApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL3_CasasVictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl3CasasVictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl3CasasVictorApplication.class, args);
+	}
+
+}
