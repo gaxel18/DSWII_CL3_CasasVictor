81749df357bdc1d5008ddba96910290f1fbdeab8
gaxel18 <alejandro_cs_09@hotmail.com>
2023-11-25 22:03:27 -0500

    configuracion inicial

--- src/main/java/pe/edu/cibertec/DSWII_CL3_CasasVictor/DswiiCl3CasasVictorApplication.java
+++ src/main/java/pe/edu/cibertec/DSWII_CL3_CasasVictor/DswiiCl3CasasVictorApplication.java
@@ -2,6 +2,9 @@ package pe.edu.cibertec.DSWII_CL3_CasasVictor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DswiiCl3CasasVictorApplication {
@@ -10,4 +13,17 @@ public class DswiiCl3CasasVictorApplication {
 		SpringApplication.run(DswiiCl3CasasVictorApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer configCorsGlobal(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("api/v1/**")
+						.allowedMethods("GET", "POST", "PUT")
+						.allowedOrigins("*");
+			}
+		};
+	}
+
 }
+
